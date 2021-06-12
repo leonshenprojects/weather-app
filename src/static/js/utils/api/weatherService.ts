ba767c29053eb14da037c87fc686c0ae97de44ce
Leon Shen <leonshen@decorrespondent.nl>
2021-06-12 10:44:39 +0200

    Creates weather api service and types response.

---
+++ src/static/js/utils/api/weatherService.ts
@@ -0,0 +1,17 @@
+import axios, { AxiosResponse } from 'axios';
+import { WeatherApiResponse } from '../types';
+
+export const getWeatherData = async () => {
+    try {
+        const response: AxiosResponse<WeatherApiResponse> = await axios.get('/data/2.5/forecast', {
+            params: {
+                q: 'M%C3%BCnchen,DE',
+                appid: 'b6907d289e10d714a6e88b30761fae22'
+            }
+        });
+
+        return response;
+    } catch (error) {
+        console.log(error);
+    }
+}
