1da111b445689d4f78ecc98ffb4005f878718c1b
Leon Shen <leonshen@decorrespondent.nl>
2021-06-13 18:44:45 +0200

    Creates WeatherForecast view.

--- src/App.tsx
+++ src/App.tsx
@@ -1,83 +1,10 @@
-import React, { useEffect, useRef, useState } from 'react';
-import * as Api from './static/js/utils/api/weatherService';
-import { WeatherItem } from './static/js/utils/types';
-import WeatherListItem from './components/WeatherListItem/WeatherListItem';
-import WeatherOverview from './components/WeatherOverview/WeatherOverview';
-import { getTime } from './static/js/utils/dateTime';
-import getTempRange, { TempRange } from './static/js/utils/getTempRange';
+import React from 'react';
+import WeatherForecast from './views/WeatherForecast/WeatherForecast'
 import './App.scss';
 
 function App() {
-    const [weatherList, setWeatherList] = useState([] as Array<WeatherItem>);
-    const [isLoading, setIsLoading] = useState(false);
-    const [selectedItem, setSelectedItem] = useState(null as WeatherItem | null);
-    const [tempRange, setTempRange] = useState({} as TempRange);
-
-    const cityName = useRef('');
-
-    const handleItemClick = (item: any) => {
-        setSelectedItem(item)
-    }
-
-    useEffect(() => {
-        async function fetchData() {
-            setIsLoading(true);
-            const response = await Api.getWeatherData();
-
-            if (response) {
-                setWeatherList(response.data.list);
-                cityName.current = response.data.city.name;
-            }
-            setIsLoading(false);
-        }
-
-        fetchData();
-    }, []);
-
-    useEffect(() => {
-        if (weatherList.length > 0) {
-            setSelectedItem(weatherList[0])
-            setTempRange(getTempRange(weatherList));
-        }
-    }, [weatherList]);
-
     return (
-        <div className="WeatherForecastApp">
-            {!isLoading &&
-                <>
-                    {selectedItem &&
-                        <WeatherOverview
-                            cityName={cityName.current}
-                            dateTime={selectedItem.dt_txt}
-                            description={selectedItem.weather[0].main}
-                            icon={selectedItem.weather[0].icon}
-                            temp={selectedItem.main.temp}
-                            tempMax={tempRange.maxTemp}
-                            tempMin={tempRange.minTemp}
-                        />
-                    }
-
-                    <ul className="WeatherForecastApp__list">
-                        {weatherList.map((item) => {
-                            return (
-                                <button
-                                    className={`WeatherForecastApp__listItem ${item.dt === selectedItem?.dt ? "isActive" : ""}`}
-                                    key={item.dt}
-                                    onClick={() => handleItemClick(item)}
-                                >
-                                    <WeatherListItem
-                                        description={item.weather[0].main}
-                                        icon={item.weather[0].icon}
-                                        temp={item.main.temp}
-                                        time={getTime(item.dt_txt)}
-                                    />
-                                </button>
-                            );
-                        })}
-                    </ul>
-                </>
-            }
-        </div>
+        <WeatherForecast/>
     );
 }
 
